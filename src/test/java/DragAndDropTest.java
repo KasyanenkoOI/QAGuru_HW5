4d8ba57d70791b94c37255f1fc0a0e51785b5f64
OIKasyanenko <severtko@mail.ru>
2023-09-25 22:54:23 +0300

    replace from dragAndDropTo with dragAndDrop

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDropTest {
@@ -15,7 +16,7 @@ public class DragAndDropTest {
     @Test
     void testdragAndDropTo() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
-        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").dragAndDrop(to("#column-b"));
         $("#column-b").shouldHave(text("a"));
         $("#column-a").shouldHave(text("b"));
     }
